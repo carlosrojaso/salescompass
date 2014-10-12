9eeb86c3d802ec146a0bd5c7a81384746ce0677f
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-10-12 04:19:02 -0700

    pages are able to be added

--- admin/js/admin_app.js
+++ admin/js/admin_app.js
@@ -8,25 +8,60 @@ angular.module('admin', ['starter.services', 'ngResource'])
 
 .controller('AdminCtrl', function($http, $window, $scope, SocketIO, Question, Answer) {
 
+  $scope.scripts = {};
+  $scope.pages = {};
+  $scope.responses = {};
 
   $scope.init = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: "/resource/scripts"
       }).success(function(data) {
         $scope.scripts = data;
-      });
+    });
+    $http({
+        method: 'GET',
+        url: "/resource/pages"
+      }).success(function(data) {
+        $scope.pages = data;
+    });
+    $http({
+        method: 'GET',
+        url: "/resource/answers"
+      }).success(function(data) {
+        $scope.answers = data;
+    });
   };
 
   $scope.saveScript = function() {
-      return $http({
-          method: 'POST',
-          data: $scope.script,
-          url: "/resource/scripts"
-        }).success(function(data) {
-            $scope.scripts.push(data);
-        });
-      };
+    return $http({
+        method: 'POST',
+        data: $scope.script,
+        url: "/resource/scripts"
+      }).success(function(data) {
+          $scope.scripts.push(data);
+      });
+  };
+
+  $scope.addPage = function() {
+    return $http({
+        method: 'POST',
+        data: $scope.page,
+        url: "/resource/pages"
+      }).success(function(data) {
+          $scope.pages.push(data);
+      });
+  };
+
+  $scope.addAnswer = function() {
+    return $http({
+        method: 'POST',
+        data: $scope.answer,
+        url: "/resource/answers"
+      }).success(function(data) {
+          $scope.answers.push(data);
+      });
+  };
 
 
 // function resetCreateForm() {
--- server/models.js
+++ server/models.js
@@ -22,17 +22,17 @@ module.exports = function(bookshelf) {
     }
   });
 
-  var Question = bookshelf.Model.extend({
-    tableName: 'questions'
+  var Page = bookshelf.Model.extend({
+    tableName: 'pages'
   });
 
   var Answer = bookshelf.Model.extend({
     tableName: 'answers'
   });
 
-  // var Script = bookshelf.Model.extend({
-  //   tableName: 'scripts'
-  // });
+  var Script = bookshelf.Model.extend({
+    tableName: 'scripts'
+  });
 
   // function save_script(req, res) {
   //   var script_id = req.params.scriptId;
@@ -48,68 +48,68 @@ module.exports = function(bookshelf) {
   //   });
   // }
 
-  function activate_question(req, res, next) {
-    var question_id = req.params.questionId;
-    console.log("activating question ", question_id);
-
-    return bookshelf.knex('questions').update({
-      'show': knex.raw('(id=' + question_id + ')')
-    }).then(function() {
-      res.send('OK');
-    }).catch(function(err) {
-      console.log("Error ", err);
-      res.status(500).send(err);
-    });
-  }
-
-  function next_question(req, res, next) {
-    var idWhere = '(select min(id) from questions where (show = false or show is null) and id > (select max(id) from questions where show = true))';
-    return bookshelf.knex('questions').update({
-        'show': bookshelf.knex.raw('(id = ' + idWhere + ')')
-      })
-      .then(function() {
-        res.send('OK');
-      }).catch(next);
-  }
-
-  function leaders(req, res, next) {
-    return bookshelf.knex('users').orderBy('points', 'desc')
-      .select('*').then(function(rows) {
-        res.json(rows);
-      });
-  }
-
-  function clear_leaders(req, res, next) {
-    var knex = bookshelf.knex;
-    knex('answers').del().then(function() {
-      knex('users').update({
-        points: 0
-      }).then(function() {
-        knex('questions').update({
-          show: false
-        }).then(function() {
-          knex('questions').where({
-            question: 'start'
-          }).update({
-            show: true
-          }).then(function() {
-            res.send('OK');
-          });
-        });
-      });
-    }).catch(function(err) {
-      next(err);
-    });
-  }
+  // function activate_question(req, res, next) {
+  //   var question_id = req.params.questionId;
+  //   console.log("activating question ", question_id);
+  //
+  //   return bookshelf.knex('questions').update({
+  //     'show': knex.raw('(id=' + question_id + ')')
+  //   }).then(function() {
+  //     res.send('OK');
+  //   }).catch(function(err) {
+  //     console.log("Error ", err);
+  //     res.status(500).send(err);
+  //   });
+  // }
+  //
+  // function next_question(req, res, next) {
+  //   var idWhere = '(select min(id) from questions where (show = false or show is null) and id > (select max(id) from questions where show = true))';
+  //   return bookshelf.knex('questions').update({
+  //       'show': bookshelf.knex.raw('(id = ' + idWhere + ')')
+  //     })
+  //     .then(function() {
+  //       res.send('OK');
+  //     }).catch(next);
+  // }
+  //
+  // function leaders(req, res, next) {
+  //   return bookshelf.knex('users').orderBy('points', 'desc')
+  //     .select('*').then(function(rows) {
+  //       res.json(rows);
+  //     });
+  // }
+  //
+  // function clear_leaders(req, res, next) {
+  //   var knex = bookshelf.knex;
+  //   knex('answers').del().then(function() {
+  //     knex('users').update({
+  //       points: 0
+  //     }).then(function() {
+  //       knex('questions').update({
+  //         show: false
+  //       }).then(function() {
+  //         knex('questions').where({
+  //           question: 'start'
+  //         }).update({
+  //           show: true
+  //         }).then(function() {
+  //           res.send('OK');
+  //         });
+  //       });
+  //     });
+  //   }).catch(function(err) {
+  //     next(err);
+  //   });
+  // }
 
   return {
     User: User,
-    Question: Question,
+    Page: Page,
     Script: Script,
-    Answer: Answer,
-    activate_question: activate_question,
-    next_question: next_question,
-    leaders: leaders,
-    clear_leaders: clear_leaders
+    Answer: Answer
+    // activate_question: activate_question,
+    // next_question: next_question,
+    // leaders: leaders,
+    // clear_leaders: clear_leaders
   };
 };
