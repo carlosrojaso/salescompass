1b2607d1216afa18719ca124f28f983ca8ac1e80
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2014-10-12 01:18:08 -0700

    icons, Facebook and i dunno what else...

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -5,6 +5,20 @@ angular.module('starter.controllers', [])
     RegistrationService.logout();
     $location.path("/register");
   }
+
+  $scope.fbLogin = function() {
+    openFB.login(
+        function(response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        },
+        {scope: 'email,publish_actions'});
+  }
+
   $scope.timeleft = '0 secs';
 })
 
